fb2bccbb089aa3b89ed887ac260ce04e8236ae4d
William Aeberhard <william.aeberhard@gmail.com>
2014-12-18 17:36:39 +0100

    added batch file for automatic test of the class

---
+++ dev/update-test.bat
@@ -0,0 +1,23 @@
+set classname=gsemthesis
+
+
+rem generates new class files
+cd src/
+if exist %classname%.cls (
+    del %classname%.cls
+)
+latex %classname%.ins
+pdflatex %classname%.dtx
+
+rem copy files into the /cls directory
+cd ..
+cp src/gsemthesis.cls cls/
+cp src/gsemthesis.pdf cls/
+
+rem test example file
+cp src/gsemthesis.cls example/
+cd example/
+pdflatex phdthesis-example.tex rem better to run it manually
+biber phdthesis-example
+
+cd ..
