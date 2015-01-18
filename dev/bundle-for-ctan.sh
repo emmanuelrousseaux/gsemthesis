1716a990e23fe204fc31c28c09cd3a058617981d
Emmanuel Rousseaux <emmanuel.rousseaux@gmail.com>
2015-01-18 21:07:28 +0100

    for a CTAN release put files in a 'gsemthesis' folder and added the README file

--- dev/bundle-for-ctan.sh
+++ dev/bundle-for-ctan.sh
@@ -1,4 +1,7 @@
-cd src
-zip gsemthesis gsemthesis.ins gsemthesis.dtx gsemthesis.pdf
-mv gsemthesis.zip ../
-cd ..
+mkdir gsemthesis
+cp README.md gsemthesis/README
+cp src/gsemthesis.ins gsemthesis/
+cp src/gsemthesis.dtx gsemthesis/
+cp src/gsemthesis.pdf gsemthesis/
+zip -r gsemthesis.zip gsemthesis
+rm -r gsemthesis
